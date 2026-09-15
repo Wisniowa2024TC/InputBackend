72f5c3ce2ea0f75145e642e165f4175ae669d637
K1 <K1@example.com>
2026-09-15 11:12:45 +0200

    MariaDB connection

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.mariadb.jdbc:mariadb-java-client:3.5.10")
 }
 
 tasks.test {
--- src/main/java/io/github/wisniowa2024tc/Main.java
+++ src/main/java/io/github/wisniowa2024tc/Main.java
@@ -1,7 +1,26 @@
 package io.github.wisniowa2024tc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
+    public Connection connection;
+
+    private static final String DB = "InputBackend";
+    private static final String USER = "IBUser";
+    private static final String PASSWORD = "PleaseDontUse";
+
+
+    public Main() {
+        try {
+            connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/" + DB + "?user=" + USER + "&password=" + PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to connect to database", e);
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
