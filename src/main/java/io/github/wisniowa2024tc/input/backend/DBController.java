2af5b84567050a81d28ed73be6b935529167682f
K1 <K1@example.com>
2026-09-15 11:43:06 +0200

    File reorganization

--- src/main/java/io/github/wisniowa2024tc/input/backend/DBController.java
+++ src/main/java/io/github/wisniowa2024tc/input/backend/DBController.java
@@ -1,26 +1,21 @@
-package io.github.wisniowa2024tc;
+package io.github.wisniowa2024tc.input.backend;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class Main {
+public class DBController {
     public Connection connection;
 
     private static final String DB = "InputBackend";
     private static final String USER = "IBUser";
     private static final String PASSWORD = "PleaseDontUse";
 
-
-    public Main() {
+    public DBController() {
         try {
             connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/" + DB + "?user=" + USER + "&password=" + PASSWORD);
         } catch (SQLException e) {
             throw new RuntimeException("Failed to connect to database", e);
         }
-    }
-
-    public static void main(String[] args) {
-
     }
 }
---
+++ src/main/java/io/github/wisniowa2024tc/input/backend/Main.java
@@ -0,0 +1,7 @@
+package io.github.wisniowa2024tc.input.backend;
+
+public class Main {
+    public static void main(String[] args) {
+        DBController db = new DBController();
+    }
+}
